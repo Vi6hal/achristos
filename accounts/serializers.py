add6fed1528a3105e779535c2943b8f74716009a
Noah <20889199+n0tNoah@users.noreply.github.com>
2021-12-09 02:33:39 +0530

    first commit

---
+++ accounts/serializers.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from django.db.models import fields
+from django.utils.timezone import make_aware
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+from djoser.conf import settings
+from djoser import utils
+from .models import UserProfile
+User = get_user_model()
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("address",)
+
+class UserSerializer(serializers.ModelSerializer):
+
+    profile = ProfileSerializer()
+    class Meta:
+        model = User
+        fields = ("profile","first_name","last_name","email","id")
+        ref_name = "UserProfile"
---
+++ invites/serializers.py
@@ -0,0 +1,96 @@
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError, transaction
+from rest_framework import serializers
+from djoser.conf import settings as djoser_settings
+User = get_user_model()
+
+class UserInviteSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            djoser_settings.USER_ID_FIELD,
+            djoser_settings.LOGIN_FIELD,
+        )
+
+    def create(self, validated_data):
+        try:
+            user = self.perform_create(validated_data)
+        except IntegrityError:
+            self.fail("cannot_create_user")
+
+        return user
+
+    def perform_create(self, validated_data):
+        with transaction.atomic():
+            user = User.objects.create_user(**validated_data, password=None)
+            if djoser_settings.SEND_ACTIVATION_EMAIL:
+                user.is_active = False
+                user.save(update_fields=["is_active"])
+        return user
+
+
+class AcceptInviteSeralizer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+
+    default_error_messages = {
+        "invalid_token": djoser_settings.CONSTANTS.messages.INVALID_TOKEN_ERROR,
+        "invalid_uid": djoser_settings.CONSTANTS.messages.INVALID_UID_ERROR,
+        "setup_complete": "You can now continue to login"
+    }
+
+    def is_setup_complete(self) -> bool:
+        """
+        To check if the us has completed the invitation workflow
+        i.e accept invite and setup password
+
+        @returns:
+            True: If user has a usable password, is_active & accepted_invite flags are true
+            False: Does not have a usable password
+
+        @depends:
+            self.user
+        """
+        if not self.user:
+            return False
+        if self.user.has_usable_password() and self.user.is_active and self.user.activated:
+            key_error = "setup_complete"
+            raise ValidationError(
+                {"uid": [self.error_messages[key_error]]}, code=key_error
+            )
+            return True
+        else:
+            return False
+
+    def validate(self, attrs):
+        validated_data = super().validate(attrs)
+        try:
+            uid = utils.decode_uid(self.initial_data.get("uid", ""))
+            self.user = User.objects.get(pk=uid)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            key_error = "invalid_uid"
+            raise ValidationError(
+                {"uid": [self.error_messages[key_error]]}, code=key_error
+            )
+        self.is_setup_complete()
+
+        is_token_valid = self.context["view"].token_generator.check_token(
+            self.user, self.initial_data.get("token", "")
+        )
+        if is_token_valid:
+            return validated_data
+        else:
+            key_error = "invalid_token"
+            raise ValidationError(
+                {"token": [self.error_messages[key_error]]}, code=key_error
+            )
+
+    def activate(self):
+        if not self.user:
+            self.fail("activation called before validation")
+        self.user.activated = make_aware(datetime.now())
+        self.user.is_active = True
+        self.user.email_confirmed = make_aware(datetime.now())
+        self.user.save()
+        return self.user
