2311fd85c9bb020ace0fa548e1665ff0a280014d
Noah <20889199+n0tNoah@users.noreply.github.com>
2021-12-09 03:35:09 +0530

    updated faker get api

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,17 @@ from rest_framework.serializers import Serializer
 from .serializers import RandomSerializer
 from rest_framework import viewsets
 from .models import RandomData
-
+import faker
 class RandomDataView(viewsets.ModelViewSet):
     queryset = RandomData.objects.all()
     serializer_class = RandomSerializer
+    def list(self, request, *args, **kwargs):
+        fake_ins = faker.Faker()
+        for i in range(1,100):
+            RandomData.objects.create(
+                **{'address':fake_ins.address(),
+                'first_name':fake_ins.first_name(),
+                'last_name':fake_ins.last_name()
+                }
+            )
+        return super().list(request, *args, **kwargs)
