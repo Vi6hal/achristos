4cbb0763d390cffef838216f57e064d7a8bdf632
Noah <20889199+n0tNoah@users.noreply.github.com>
2021-12-09 22:17:52 +0530

    update

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 from celery import shared_task
+import requests
 
 @shared_task
 def add():
-    print("AAAAA")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n running task ~~~~~~~~~~~~~~~~~~~~~~~~~")
+    requests.get("https://sobercheck.herokuapp.com/data")
+    return
--- cron.py
+++ cron.py
@@ -1,3 +1,3 @@
 import requests
 print("hello stupid")
-requests.get("https://sobercheck.herokuapp.com/data/?id__in=5")
+requests.get("https://sobercheck.herokuapp.com/data")
