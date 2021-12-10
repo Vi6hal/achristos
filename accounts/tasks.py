ce90498e2deb9e4bcaa3cd7ac80aba0bab93aa9e
Noah <20889199+n0tNoah@users.noreply.github.com>
2021-12-10 23:39:33 +0530

    updated data

--- accounts/tasks.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from celery import shared_task
-import requests
-
-@shared_task
-def add():
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n running task ~~~~~~~~~~~~~~~~~~~~~~~~~")
-    # requests.get("https://sobercheck.herokuapp.com/data")
-    return
--- cronjob.py
+++ cronjob.py
@@ -1,26 +1,31 @@
-import asyncio
-import time 
-import aiohttp
+import asyncio,time,datetime,aiohttp
 from aiohttp.client import ClientSession
+import pytz
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','achristos.settings')
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from django.utils.timezone import make_aware
+from registery.models import AppRegistery
 
-async def download_link(url:str,session:ClientSession):
+async def ping(url:str,session:ClientSession):
     async with session.get(url) as response:
-        pass
-    return
+        result = await response.text()
+        print(f'Read {len(result)} from {url}')
 
-async def download_all(urls:list):
+async def ping_all(urls:list):
     my_conn = aiohttp.TCPConnector(limit=10)
-    conn = aiohttp.ClientSession(connector=my_conn)
-    async with conn:
+    async with aiohttp.ClientSession(connector=my_conn) as session:
         tasks = []
         for url in urls:
-            task = asyncio.ensure_future(download_link(url=url,session=conn))
+            task = asyncio.ensure_future(ping(url=url,session=session))
             tasks.append(task)
-        await asyncio.gather(*tasks,return_exceptions=True)
+    await asyncio.gather(*tasks,return_exceptions=True) # the await must be nest inside of the session
 
-url_list = ["https://sobercheck.herokuapp.com/"]*100
-
+links = list(AppRegistery.objects.filter(bedtime=False).values_list('url',flat=True))
 start = time.time()
-asyncio.run(download_all(url_list))
+asyncio.run(ping_all(links))
 end = time.time()
-# print(f'download {len(url_list)} links in {end - start} seconds')
+print(f'download {len(links)} links in {end - start} seconds')
+aware_datetime = make_aware(datetime.datetime.now())
+AppRegistery.objects.filter(url__in=links).update(last_pinged=aware_datetime)
